33fc0fa1acaccb4bdd6ff25c6b328fdfecbdde2d
Peter Søgaard <pso@digital-identity.dk>
2025-10-03 15:02:57 +0200

    fixed bad migrations

--- ui/src/main/resources/db/migration/V1_122__add_master_index_on_user.sql
+++ ui/src/main/resources/db/migration/V1_122__add_master_index_on_user.sql
@@ -1,16 +1,1 @@
-# delete duplicate master + master_id rows, keeping the initial record
-DELETE u
-FROM users u
-         JOIN (
-    SELECT id
-    FROM (
-             SELECT
-                 id,
-                 ROW_NUMBER() OVER (PARTITION BY master, master_id ORDER BY id) AS rn
-             FROM users
-         ) ranked
-    WHERE rn > 1
-) dup ON u.id = dup.id;
-
-# introduce new unique indexe on master and master_id
-ALTER TABLE users ADD UNIQUE INDEX ux_users__master_master_id (master, master_id);
+# left empty on purpose (content moved to V1_125)
--- ui/src/main/resources/db/migration/V1_125__delete_orphan_users.sql
+++ ui/src/main/resources/db/migration/V1_125__delete_orphan_users.sql
@@ -3,3 +3,20 @@ DELETE u
 FROM users u
 LEFT JOIN persons_users pu ON pu.user_id = u.id
 WHERE pu.id IS NULL;
+
+# delete duplicate master + master_id rows, keeping the initial record
+DELETE u
+FROM users u
+         JOIN (
+    SELECT id
+    FROM (
+             SELECT
+                 id,
+                 ROW_NUMBER() OVER (PARTITION BY master, master_id ORDER BY id) AS rn
+             FROM users
+         ) ranked
+    WHERE rn > 1
+) dup ON u.id = dup.id;
+
+# introduce new unique indexe on master and master_id
+ALTER TABLE users ADD UNIQUE INDEX ux_users__master_master_id (master, master_id);
