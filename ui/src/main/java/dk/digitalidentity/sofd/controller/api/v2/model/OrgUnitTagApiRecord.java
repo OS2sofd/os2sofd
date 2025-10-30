57ccbfde745ce98803ff012a4ff998904cbfd808
Peter Søgaard <pso@digital-identity.dk>
2025-10-30 09:46:17 +0100

    added tagType to v2 api

--- ui/src/main/java/dk/digitalidentity/sofd/controller/api/v2/model/OrgUnitTagApiRecord.java
+++ ui/src/main/java/dk/digitalidentity/sofd/controller/api/v2/model/OrgUnitTagApiRecord.java
@@ -1,6 +1,7 @@
 package dk.digitalidentity.sofd.controller.api.v2.model;
 
 import dk.digitalidentity.sofd.dao.model.OrgUnitTag;
+import dk.digitalidentity.sofd.dao.model.enums.TagType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,9 +14,11 @@ import lombok.ToString;
 public class OrgUnitTagApiRecord {
     private String customValue;
     private String tag;
+    private TagType tagType;
 
     public OrgUnitTagApiRecord(OrgUnitTag tag) {
         this.customValue = tag.getTag().isCustomValueEnabled() ? tag.getCustomValue() : null;
         this.tag = tag.getTag().getValue();
+        this.tagType = tag.getTag().getTagType();
     }
 }
