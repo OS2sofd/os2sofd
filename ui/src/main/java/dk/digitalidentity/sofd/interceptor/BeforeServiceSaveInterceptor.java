51320380c18752a4e6867d04af5b0445ef00aee9
Peter Søgaard <pso@digital-identity.dk>
2025-12-21 12:09:20 +0100

    Fixed transaction issues in BeforeServiceSaveInterceptor

--- ui/src/main/java/dk/digitalidentity/sofd/interceptor/BeforeServiceSaveInterceptor.java
+++ ui/src/main/java/dk/digitalidentity/sofd/interceptor/BeforeServiceSaveInterceptor.java
@@ -1,14 +1,15 @@
 package dk.digitalidentity.sofd.interceptor;
 
-import org.aspectj.lang.annotation.AfterReturning;
+import dk.digitalidentity.sofd.dao.model.OrgUnit;
+import dk.digitalidentity.sofd.dao.model.Person;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import dk.digitalidentity.sofd.dao.model.OrgUnit;
-import dk.digitalidentity.sofd.dao.model.Person;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Aspect
 @Component
@@ -16,20 +17,35 @@ public class BeforeServiceSaveInterceptor {
 
 	@Autowired
 	private AbstractBeforeSaveInterceptor interceptor;
-	
-	@Transactional
-	@Before("execution(* dk.digitalidentity.sofd.service.PersonService.save(dk.digitalidentity.sofd.dao.model.Person)) && args(person)")
-	public void beforeSavePerson(Person person) {
-		interceptor.handleSavePerson(person);
+
+	@Autowired
+	private PlatformTransactionManager transactionManager;
+
+	@Around("execution(* dk.digitalidentity.sofd.service.PersonService.save(dk.digitalidentity.sofd.dao.model.Person)) && args(person)")
+	public Object aroundSavePerson(ProceedingJoinPoint joinPoint, Person person) throws Throwable {
+		TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+
+		return transactionTemplate.execute(status -> {
+			try {
+				// before save
+				interceptor.handleSavePerson(person);
+
+				// actual save
+				Person result = (Person) joinPoint.proceed();
+
+				// after save
+				interceptor.handleAccountOrders(result);
+
+				return result;
+			} catch (Throwable e) {
+				status.setRollbackOnly();
+				throw new RuntimeException(e);
+			}
+		});
 	}
-	
+
 	@Before("execution(* dk.digitalidentity.sofd.service.OrgUnitService.save(dk.digitalidentity.sofd.dao.model.OrgUnit)) && args(orgUnit)")
 	public void beforeSaveOrgUnit(OrgUnit orgUnit) {
 		interceptor.handleSaveOrgUnit(orgUnit);
-	}
-
-	@AfterReturning(pointcut = "execution(* dk.digitalidentity.sofd.service.PersonService.save(dk.digitalidentity.sofd.dao.model.Person)) && args(person)", returning = "retVal")
-	public void handleAfterSavePerson(Person person, Person retVal) {
-		interceptor.handleAccountOrders(retVal);
 	}
 }
