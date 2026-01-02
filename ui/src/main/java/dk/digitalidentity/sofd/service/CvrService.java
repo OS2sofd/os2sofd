c6783563a9fd18c08b40483938fc59647db820f5
Brian Storm Graversen <bsg@digital-identity.dk>
2026-01-02 08:12:40 +0100

    make sure eans are loaded during CVR maintanence

--- ui/src/main/java/dk/digitalidentity/sofd/service/CvrService.java
+++ ui/src/main/java/dk/digitalidentity/sofd/service/CvrService.java
@@ -57,6 +57,7 @@ public class CvrService {
 		for (OrgUnit orgUnit : orgUnitService.getAll(o -> {
 			o.getPostAddresses().stream().forEach(pa -> pa.getPost().getMasterId());
 			o.getPhones().stream().forEach(p -> p.getPhone().getPhoneNumber());
+			o.getEanList().forEach(e -> e.getNumber());
 		})) {
 			boolean changes = false;
 			List<Post> postsWithMasterCvr = OrgUnitService.getPosts(orgUnit).stream().filter(p -> p.getMaster().equals("CVR")).collect(Collectors.toList());
