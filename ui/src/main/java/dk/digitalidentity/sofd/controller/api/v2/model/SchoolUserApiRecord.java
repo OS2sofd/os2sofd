d2b3535808b11c6cd26ace2072b6aa696e9ba224
Peter Søgaard <pso@digital-identity.dk>
2026-03-06 09:36:07 +0100

    school user api now returns primary stil role as title if present

--- ui/src/main/java/dk/digitalidentity/sofd/controller/api/v2/model/SchoolUserApiRecord.java
+++ ui/src/main/java/dk/digitalidentity/sofd/controller/api/v2/model/SchoolUserApiRecord.java
@@ -32,12 +32,18 @@ public class SchoolUserApiRecord extends BaseRecord {
 	private String cpr;
 	private List<String> institutionNumbers;
 
+	// if a municipality wants a different priority at some point, then move this setting to municipality entity in db
+	private static final List<String> ROLE_PRIORITY = List.of(
+			"Lærer", "TAP", "Ledelse", "Leder", "Pædagog", "Vikar", "Konsulent"
+	);
+
+
 	public SchoolUserApiRecord(SchoolUser schoolUser) {
 		this.uuid = schoolUser.getUuid();
 		this.username = schoolUser.getUserId();
 		this.disabled = schoolUser.isDisabled();
 		this.name = schoolUser.getName();
-		this.title = schoolUser.getTitle();
+		this.title = getTitle(schoolUser);
 		this.cpr = schoolUser.getCpr();
 		this.institutionNumbers = getInstitutionNumbers(schoolUser.getLocalExtensions());
 	}
@@ -58,4 +64,30 @@ public class SchoolUserApiRecord extends BaseRecord {
 		}
 		return result;
 	}
+
+	private String getTitle(SchoolUser schoolUser) {
+		var result = schoolUser.getTitle();
+		try {
+			Map<String, String> map = OBJECT_MAPPER.readValue(schoolUser.getLocalExtensions(), Map.class);
+			// Roller is a local extension that can be set by the STIL integration
+			var institutionNumbers = map.get("Roller");
+			if (institutionNumbers != null) {
+				var allRoles = List.of(institutionNumbers.split(","));
+
+				var primaryRole = ROLE_PRIORITY.stream()
+						.filter(allRoles::contains)
+						.findFirst()
+						.orElse(null);
+
+				if (primaryRole != null) {
+					result = primaryRole;
+				}
+			}
+		}
+		catch (Exception e) {
+			// ignore
+		}
+		return result;
+	}
+
 }
