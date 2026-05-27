e560a2dca4d98df3f5835b9ccb9c37f0278569f1
Brian Storm Graversen <bsg@digital-identity.dk>
2026-05-27 10:09:58 +0200

    fix to scheduler so it remains single threaded

--- ui/src/main/java/dk/digitalidentity/sofd/config/SchedulerConfig.java
+++ ui/src/main/java/dk/digitalidentity/sofd/config/SchedulerConfig.java
@@ -1,9 +1,10 @@
 package dk.digitalidentity.sofd.config;
 
-import java.util.concurrent.Executors;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 // virtual threads enables SimpleAsyncTaskScheduler, which allows the same task to run multiple times (as a side-effect), and we do NOT want
@@ -11,8 +12,22 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
 
+	@Bean(destroyMethod = "shutdown")
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("scheduled-");
+        scheduler.setRemoveOnCancelPolicy(true);
+
+        // explicitly do NOT use virtual threads
+        scheduler.setVirtualThreads(false);
+        scheduler.initialize();
+
+        return scheduler;
+    }
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-        taskRegistrar.setScheduler(Executors.newSingleThreadScheduledExecutor());
+        taskRegistrar.setTaskScheduler(taskScheduler());
     }
 }
